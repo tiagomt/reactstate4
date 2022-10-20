30e22d3a8dad37bda4c72049bd3d3b2789b85fe8
Tiago 3 <tiago-tmt@hotmail.com>
2022-10-20 15:35:48 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./style.css"
+
+export default function App() {
+  const [isGoingOut, setIsGoingOut] = React.useState(true)
+
+  function changeMind() {
+    setIsGoingOut(prevState => !prevState)
+  }
+
+  return (
+    <div className="state">
+      <h1 className="state--title">Do I feel like going out tonight?</h1>
+      <div onClick={changeMind} className="state--value">
+        <h1>{isGoingOut ? "Yes" : "No"}</h1>
+      </div>
+    </div>
+  )
+}
